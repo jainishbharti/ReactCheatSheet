d29620dfcdf5e0beeb113cd71b7f728acaaa2e2a
Jainish-Bharti <jainishbharti@datagrokr.com>
2022-06-10 20:15:06 +0530

    created a formik-control which makes usage of formik easy

---
+++ formik-controls/src/components/CheckboxGroup.js
@@ -0,0 +1,42 @@
+import { Field, ErrorMessage } from "formik";
+import React from "react";
+import { TextError } from "./TextError";
+
+export const CheckboxGroup = (props) => {
+  const { label, name, options, ...rest } = props;
+  return (
+    <div className="form-control">
+      <label htmlFor={name}>{label}</label>
+      <Field name={name} {...rest}>
+        {({ field }) => {
+          return options.map((option) => {
+            return (
+              <React.Fragment key={option.key}>
+                <input
+                  type="checkbox"
+                  id={option.value}
+                  {...field}
+                  {...rest}
+                  value={option.value}
+                  checked={field.value.includes(option.value)}
+                />
+                <label
+                  style={{
+                    display: "inline",
+                    marginRight: "15px",
+                    marginLeft: "5px",
+                  }}
+                  htmlFor={option.value}
+                >
+                  {option.key}
+                </label>
+              </React.Fragment>
+            );
+          });
+        }}
+      </Field>
+      <ErrorMessage name={name} component={TextError} />
+    </div>
+  );
+};
+
